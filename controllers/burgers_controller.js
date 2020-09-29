c677c1f0652a658d411022611d42b41b976c7581
rudy <argmend@gmail.com>
2020-09-28 23:51:16 -0500

    burgers_controller.js updated.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+// Dependency
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger");
+
+// Root folder
+router.get("/", (req, res) => {
+    // console.log(req);
+    burger.selectAll(data => {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// burger
+router.post("/api/burger", (req, res) => {
+    burger.insertOne("burger_name", req.body.burger_name, result => {
+        res.json({id: result.insertId});
+    });
+});
+
+// burger/id
+router.put("/api/burger/:id", (req, res) => {
+
+    var burgerId = req.params.id;
+  
+    burger.updateOne(burgerId, result => {
+      if (result.changedRows == 0) {
+
+        // If no rows were changed, then 404
+
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+// Export routes for server.js to use.
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
-const express = require("express");
+var express = require('express');
 
-const PORT = process.env.PORT || 8080;
+// Listening on Port 8080
+var PORT = process.env.PORT || 8080;
 
-const app = express();
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -11,22 +12,22 @@ app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/public", express.static("./public"));
 
 // Set Handlebars.
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-
+    
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
-
 });
