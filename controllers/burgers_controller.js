5b992bd9c27c1cf8cea00fc2872141186a13b0d1
rudy <argmend@gmail.com>
2020-10-03 23:25:57 -0500

    Push to Heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,7 +18,7 @@ router.get("/", (req, res) => {
 
 // burger
 router.post("/api/hamburger", (req, res) => {
-    burger.insertOne("burger_name", req.body.burger_name, result => {
+    burger.create("burger_name", req.body.burger_name, result => {
         res.json({id: result.insertId});
     });
 });
@@ -28,7 +28,7 @@ router.put("/api/hamburger/:id", (req, res) => {
 
     var burgerId = req.params.id;
   
-    burger.updateOne(burgerId, result => {
+    burger.update(burgerId, result => {
       if (result.changedRows == 0) {
 
         // If no rows were changed, then 404
